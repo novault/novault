6c8399dc0f4872dc5f400f0e503ee202a6bdca85
avnr <github@herskovits.com>
2016-11-21 13:00:26 +0200

    issue #5: Add sillyname, birthdate to API

--- novault/__init__.py
+++ novault/__init__.py
@@ -1,8 +1,8 @@
 from os. path import dirname, join as path_join
 
-from .novault import novault, mk_seed, password, btc, xmr, COINS
+from .novault import novault, mk_seed, sillyname, birthdate, password, btc, xmr, COINS
 
-__all__ = [ 'novault', 'mk_seed', 'password', 'btc', 'xmr', 'COINS', '__version__' ]
+__all__ = [ 'novault', 'mk_seed', 'password', 'sillyname', 'birthdate', 'btc', 'xmr', 'COINS', '__version__' ]
 
 with open( path_join( dirname( __file__ ), '__version__' )) as f:
     __version__ = f. read()
